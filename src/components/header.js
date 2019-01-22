06957957b7fd96f5c7874b0ccd9aeff59222c21c
MikeKalinowski <MikeKalinowski@users.noreply.github.com>
2019-01-22 18:21:35 +0100

    Hamburger

--- src/components/header.js
+++ src/components/header.js
@@ -10,8 +10,23 @@ const Header = () => (
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M21.5 19C21.7761 19 22 18.7761 22 18.5V17.5C22 17.2239 21.7761 17 21.5 17H2.5C2.22386 17 2 17.2239 2 17.5V18.5C2 18.7761 2.22386 19 2.5 19H21.5ZM22 11.5C22 11.2239 21.7761 11 21.5 11H2.5C2.22386 11 2 11.2239 2 11.5V12.5C2 12.7761 2.22386 13 2.5 13H21.5C21.7761 13 22 12.7761 22 12.5V11.5ZM22 5.5C22 5.22386 21.7761 5 21.5 5H2.5C2.22386 5 2 5.22386 2 5.5V6.5C2 6.77614 2.22386 7 2.5 7H21.5C21.7761 7 22 6.77614 22 6.5V5.5Z" fill="#333333"/>
       </svg>
+      <input type="checkbox" />
+      <ul>
+       <li>
+        <a href="#">Show your work</a>
+       </li>
+       <li>
+        <a href="#">Our work flow</a>
+       </li>
+       <li>
+        <a href="#">Our offer</a>
+       </li>
+       <li>
+        <a href="#">Contact</a>
+       </li>
+      </ul>
     </div>
-    <div>
+    <div className="logoName">
       <Link to="/">
         Creation
         <span className="green">
